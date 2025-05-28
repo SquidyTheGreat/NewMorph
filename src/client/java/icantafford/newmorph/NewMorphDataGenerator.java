3ca15dedc81729de90c7a3d9fb2131f04497df4c
Cassia <techandscience0501@gmail.com>
2025-05-27 21:55:44 -0500

    First commmit

---
+++ src/client/java/icantafford/newmorph/NewMorphDataGenerator.java
@@ -0,0 +1,11 @@
+package icantafford.newmorph;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class NewMorphDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
